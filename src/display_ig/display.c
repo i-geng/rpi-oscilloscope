53f09b5e4bf53efd5dccec7dc68fe30d7d5b6e9d
Irene Geng <irenegeng2@berkeley.edu>
2025-02-26 19:28:07 -0800

    function to draw horizontal line

--- src/display_ig/display.c
+++ src/display_ig/display.c
@@ -1,6 +1,6 @@
 #include "rpi.h"
-#include "i2c.h"
 #include "display.h"
+#include "i2c.h"
 
 static uint8_t buffer[1 + DISPLAY_BUFFER_SIZE];
 static uint8_t *display_buffer = buffer + 1;
@@ -17,27 +17,27 @@ void display_init(void) {
 
   // 1. Set multiplex ratio
   display_send_command(0xA8);
-  display_send_command(0x3F);   // use offset 63 because display is 64 pixels tall
+  display_send_command(0x3F); // use offset 63 because display is 64 pixels tall
 
   // 2. Set display offset [SSD1306 pg 37]
-  display_send_command(0xD3);   // double byte command
-  display_send_command(0x00);   // map display start line to COM0 [SSD1306 pg 37]
+  display_send_command(0xD3); // double byte command
+  display_send_command(0x00); // map display start line to COM0 [SSD1306 pg 37]
 
   // 3. Set display start line [SSD1306 pg 36]
-  display_send_command(0x40);   // RAM row 0 is mapped to COM0
+  display_send_command(0x40); // RAM row 0 is mapped to COM0
 
   // 4. Set segment re-map [SSD1306 pg 36]
-  display_send_command(0xA0);   // column addr 0 is mapped to SEG0
+  display_send_command(0xA0); // column addr 0 is mapped to SEG0
 
   // 5. Set COM output scan direction
-  display_send_command(0xC8);   // COM normal [SSD1306 pg 38]
+  display_send_command(0xC8); // COM normal [SSD1306 pg 38]
 
   // 6. Set COM pins hardware configuration [SSD1306 pg 40]
   display_send_command(0xDA);
-  display_send_command(0x12);   // [SSD1206 pg 11] diagram says to use 0x12
+  display_send_command(0x12); // [SSD1206 pg 11] diagram says to use 0x12
 
   // 7. Set contrast control [SSD1306 pg 36]
-  display_send_command(0x81);   // double byte command
+  display_send_command(0x81); // double byte command
   // the chip has 256 contrast steps from 00h to FFH
   // recommended value seems to be 127 (middle of the range)
   display_send_command(0x7F);
@@ -47,28 +47,28 @@ void display_init(void) {
   // display_send_command(0xA5);
 
   // 9. Set normal or inverse display [SSD1306 pg 37]
-  display_send_command(0xA6);   // normal
+  display_send_command(0xA6); // normal
   // display_send_command(0xA7);  // inverse
 
   // 10. Set display clock divide ratio/oscillator frequency [SSD1306 pg 40]
-  display_send_command(0xD5);   // double byte command
-  display_send_command(0x80);   // default value ... formula on pg 22
+  display_send_command(0xD5); // double byte command
+  display_send_command(0x80); // default value ... formula on pg 22
 
   // 11. Enable charge pump regulator [SSD1306 pg 62]
-  display_send_command(0x8D);   // double byte command
-  display_send_command(0x14);   // enable charge pump
+  display_send_command(0x8D); // double byte command
+  display_send_command(0x14); // enable charge pump
 
   // 12. Specify HORIZONTAL addressing mode [SSD1306 pg 35]
   // display_send_command(0x20);
   // display_send_command(0x02);   // page addressing mode [SSD1306 pg 34]
   display_send_command(0x20);
-  display_send_command(0x00);   // horizontal addressing mode
-  display_send_command(0x21);   // set column address
-  display_send_command(0x00);     // start at column 0
-  display_send_command(0x7F);     // end at column 127
-  display_send_command(0x22);   // set page address
-  display_send_command(0x00);     // start at page 0
-  display_send_command(0x07);     // end at page 7
+  display_send_command(0x00); // horizontal addressing mode
+  display_send_command(0x21); // set column address
+  display_send_command(0x00); // start at column 0
+  display_send_command(0x7F); // end at column 127
+  display_send_command(0x22); // set page address
+  display_send_command(0x00); // start at page 0
+  display_send_command(0x07); // end at page 7
 
   // 13. Display on [SSD1306 pg 62]
   display_send_command(0xAF);
@@ -84,34 +84,66 @@ void display_send_command(uint8_t cmd) {
 }
 
 void display_show(void) {
-  buffer[0] = 0x40;   // control byte to indicate data
+  buffer[0] = 0x40; // control byte to indicate data
   i2c_write(DISPLAY_ADDRESS, buffer, sizeof(buffer));
 }
 
 void display_clear(void) {
-  buffer[0] = 0x40;   // control byte to indicate data
+  buffer[0] = 0x40; // control byte to indicate data
   memset(display_buffer, 0, DISPLAY_BUFFER_SIZE);
 }
 
 void display_fill_buffer(void) {
-  buffer[0] = 0x40;   // control byte to indicate data
+  buffer[0] = 0x40; // control byte to indicate data
   memset(display_buffer, 255, DISPLAY_BUFFER_SIZE);
 }
 
 void display_draw_pixel(uint16_t x, uint16_t y, color_t color) {
-  // Convention: top left corner of screen is (0, 0)
-  x = DISPLAY_WIDTH - x - 1;
+  // Convention: bottom left corner of screen is pixel (0, 0)
 
   switch (color) {
-    case COLOR_WHITE:
-      display_buffer[x + (y / 8) * DISPLAY_WIDTH] |= (1 << (y & 7));
-      break;
-    case COLOR_BLACK:
-      display_buffer[x + (y / 8) * DISPLAY_WIDTH] &= ~(1 << (y & 7));
-      break;
-    case COLOR_INVERT:
-      display_buffer[x + (y / 8) * DISPLAY_WIDTH] ^= (1 << (y & 7));
-      break;
+  case COLOR_WHITE:
+    display_buffer[(y / 8) * DISPLAY_WIDTH + x] |= (1 << (y & 7));
+    break;
+  case COLOR_BLACK:
+    display_buffer[(y / 8) * DISPLAY_WIDTH + x] &= ~(1 << (y & 7));
+    break;
+  case COLOR_INVERT:
+    display_buffer[(y / 8) * DISPLAY_WIDTH + x] ^= (1 << (y & 7));
+    break;
   }
 }
 
+void display_draw_axes(void) {}
+
+// Draw a horizontal line from x_start to x_end (inclusive)
+void draw_horizontal_line(int16_t x_start, int16_t x_end, int16_t y,
+                          color_t color) {
+  x_start = x_start < 0 ? 0 : x_start;
+  x_end = x_end > DISPLAY_WIDTH - 1 ? DISPLAY_WIDTH - 1 : x_end;
+
+  if ((y >= 0) && (y < DISPLAY_HEIGHT)) {
+    int16_t w = x_end - x_start;
+    uint8_t *pBuf = &display_buffer[(y / 8) * DISPLAY_WIDTH + x_start];
+    uint8_t mask = 1 << (y & 7);
+
+    switch (color) {
+    case COLOR_WHITE:
+      while (w--) {
+        *pBuf++ |= mask;
+      };
+      break;
+    case COLOR_BLACK:
+      mask = ~mask;
+      while (w--) {
+        *pBuf++ &= mask;
+      };
+      break;
+    case COLOR_INVERT:
+      while (w--) {
+        *pBuf++ ^= mask;
+      };
+      break;
+    }
+  }
+}
--- src/display_ig/display.h
+++ src/display_ig/display.h
@@ -1,16 +1,31 @@
 #ifndef __RPI_DISPLAY_H__
 #define __RPI_DISPLAY_H__
 
-#define DISPLAY_ADDRESS 0X3C
-#define DISPLAY_WIDTH 128
-#define DISPLAY_HEIGHT 64
-#define DISPLAY_BUFFER_SIZE (DISPLAY_WIDTH * ((DISPLAY_HEIGHT + 7) / 8))
-
 typedef enum {
   COLOR_WHITE,
   COLOR_BLACK,
   COLOR_INVERT,
 } color_t;
+
+enum {
+  DISPLAY_ADDRESS = 0X3C,
+  DISPLAY_WIDTH = 128,
+  DISPLAY_HEIGHT = 64,
+  DISPLAY_BUFFER_SIZE = DISPLAY_WIDTH * ((DISPLAY_HEIGHT + 7) / 8),
+
+  // Ranges for x- and y-axes (real numbers, not pixel coordinates)
+  x_axis_min = 0,
+  x_axis_max = 100,
+  y_axis_min = 0,
+  y_axis_max = 50,
+
+  // Pixel margins around the graph
+  left_margin = 5,
+  right_margin = 0,
+  top_margin = 0,
+  bottom_margin = 5,
+
+};
 
 void display_init(void);
 
@@ -23,5 +38,7 @@ void display_draw_pixel(uint16_t x, uint16_t y, color_t color);
 void display_clear(void);
 
 void display_fill_buffer(void);
+
+void draw_horizontal_line(int16_t x_start, int16_t x_end, int16_t y, color_t color);
 
 #endif
