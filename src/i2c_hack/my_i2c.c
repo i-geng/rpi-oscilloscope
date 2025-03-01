adf9e7621ae7c2df9a5cf9d23fdd3979105dfc42
ba144220 <hs41511@gmail.com>
2025-02-28 17:09:14 -0800

    feat: i2c read (not testedgit add .git add .)

--- src/i2c_hack/my_i2c.c
+++ src/i2c_hack/my_i2c.c
@@ -1,13 +1,17 @@
 #include "rpi.h"
 #include "i2c.h"
 
+#define BSC0_BASE   0x20205000
 #define BSC1_BASE   0x20804000
-#define BSC_C       (BSC1_BASE + 0x0)   // Control register
-#define BSC_S       (BSC1_BASE + 0x4)   // Status register
-#define BSC_DLEN    (BSC1_BASE + 0x8)   // Data length register
-#define BSC_A       (BSC1_BASE + 0xC)   // Address register
-#define BSC_FIFO    (BSC1_BASE + 0x10)  // FIFO register
-#define BSC_DIV     (BSC1_BASE + 0x14)  // Clock divider register
+// #define BSC2_BASE   0x20805000
+
+#define BSC_BASE    BSC1_BASE
+#define BSC_C       (BSC_BASE + 0x0)   // Control register
+#define BSC_S       (BSC_BASE + 0x4)   // Status register
+#define BSC_DLEN    (BSC_BASE + 0x8)   // Data length register
+#define BSC_A       (BSC_BASE + 0xC)   // Address register
+#define BSC_FIFO    (BSC_BASE + 0x10)  // FIFO register
+#define BSC_DIV     (BSC_BASE + 0x14)  // Clock divider register
 
 void my_i2c_init(void) {
     dev_barrier();
@@ -34,12 +38,10 @@ void my_i2c_init_clk_div(unsigned clk_div) {
 }
 
 void my_i2c_write(unsigned addr, uint8_t data[], unsigned nbytes) {
-    putk("my_i2c_write\n");
-    // i2c_write(addr, data, nbytes);
-    // return;
-
     unsigned int remaining_bytes = nbytes;
     unsigned int data_index = 0;
+
+    dev_barrier();
     // Set the address
     PUT32(BSC_A, addr);
     // Clear the FIFO
@@ -48,7 +50,6 @@ void my_i2c_write(unsigned addr, uint8_t data[], unsigned nbytes) {
     PUT32(BSC_S, ((1 << 1) | (1 << 8) | (1 << 9)));
     // Set data length
     PUT32(BSC_DLEN, nbytes);
-    dev_barrier();
 
     // Start the transfer
     PUT32(BSC_C, (1 << 7) | (1 << 15));
@@ -63,6 +64,55 @@ void my_i2c_write(unsigned addr, uint8_t data[], unsigned nbytes) {
     }
 
     // Get status register
+    uint32_t status = GET32(BSC_S);
+    if (status & (1 << 8)) {
+        putk("ERR ACK Error\n");
+    }
+    if (status & (1 << 9)) {
+        putk("CLKT Clock Stretch Timeout\n");
+    }
+    if (remaining_bytes != 0) {
+        putk("Remaining bytes not 0\n");
+    }
+
+    // Set done bit
+    PUT32(BSC_S, (1 << 1));
+    dev_barrier();
+    return;
+}
+
+void my_i2c_read(unsigned addr, uint8_t data[], unsigned nbytes) {
+    unsigned int remaining_bytes = nbytes;
+    unsigned int data_index = 0;
+    // Set the address
+    PUT32(BSC_A, addr);
+    // Clear the FIFO
+    PUT32(BSC_C, (1 << 4));
+    // Clear the status register
+    PUT32(BSC_S, ((1 << 1) | (1 << 8) | (1 << 9)));
+    // Set data length
+    PUT32(BSC_DLEN, nbytes);
+    
+    // Start the transfer
+    PUT32(BSC_C, (1 << 7) | (1 << 15) | (1 << 0)); // Set the read bit
+
+    while(!(GET32(BSC_S) & (1 << 1))) {
+        while(remaining_bytes > 0 && GET32(BSC_S) & (1 << 5)) { // Check if the FIFO has data
+            // Read data from the FIFO
+            data[data_index] = GET32(BSC_FIFO);
+            data_index++;
+            remaining_bytes--;
+        }
+    }
+
+    // Flush the FIFO
+    while (remaining_bytes > 0 && GET32(BSC_S) & (1 << 5)) {
+        data[data_index] = GET32(BSC_FIFO);
+        data_index++;
+        remaining_bytes--;
+    }
+
+    // Check for errors
     uint32_t status = GET32(BSC_S);
     if (status & (1 << 8)) {
         putk("ERR ACK Error\n");
